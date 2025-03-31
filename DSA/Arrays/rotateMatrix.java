0fb80be494fc99f2e23aa61b83b271664854c757
Sukh <130579680+sukh285@users.noreply.github.com>
2025-03-31 21:49:43 +0530

    Add files via upload

---
+++ DSA/Arrays/rotateMatrix.java
@@ -0,0 +1,29 @@
+class rotateMatrix {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int i = 0; i < n ; i++){
+            for(int j = i+1; j < n; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                 matrix[j][i] = temp;
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            int left = 0, right = n - 1;
+
+        while (left < right) {
+            // Swap left and right elements
+            int temp = matrix[i][left];
+            matrix[i][left] = matrix[i][right];
+            matrix[i][right] = temp;
+
+            // Move pointers inward
+            left++;
+            right--;
+        }
+
+        }
+        return;
+    }
+}
